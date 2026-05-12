fd2ba1a20345df73428392e2a906941ee16cf5c1
Aline-mutesi <158449221+Mutesi-alin@users.noreply.github.com>
2026-05-12 09:23:44 +0200

    Update serializers.py

--- api/serializers.py
+++ api/serializers.py
@@ -1,42 +1,4 @@
-# from rest_framework import serializers
-# from student.models import Student
-# from course.models import Course
-# from classes.models import Classes
-# from teacher.models import Teacher
-# from  classperiod.models  import  ClassPeriod
 
-# class StudentSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Student
-#         fields = '__all__'
-
-# class TeacherSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Teacher
-#         fields = '__all__'
-
-# class CourseSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Course
-#         fields = '__all__'
-
-# class ClassesSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Classes
-#         fields = '__all__'
-
-# class ClassPeriodSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = ClassPeriod
-#         fields = '__all__'
-
-# class MinimalStudentSerializer(serializers.ModelSerializer):
-#     full_name= serializers.SerializerMethodField()
-#     def get_full_name(self,object):
-#         return f"{object.first_name} {object.last_name}"
-#     class Meta:
-#         model=Student
-#         fields=["id","full_name","email"]
 
 
 from rest_framework import serializers
